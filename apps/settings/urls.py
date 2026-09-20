ec7a1dfcb4bd64443e5d65cb38384216395fa2ea
Praveen Maurya <praveenmaurya@Praveens-MacBook-Air-2.local>
2026-09-20 17:08:48 +0530

    fake commit

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from apps.settings.views import LegalSettingsView
 
 # /api/settings/
+# comment
 urlpatterns = [
     path("legal/", LegalSettingsView.as_view(), name="settings-legal"),
 ]
